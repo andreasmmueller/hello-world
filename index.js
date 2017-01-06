757039147abbbac532f78c3bf5c1e34a287f2334
andreasmmueller <andreas.mueller@ieee.org>
2017-01-06 14:07:02 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
-    // set timezone as appropriate
+    // set NZ timezone
     currentTime.setTimezone("Pacific/Auckland");
     callback(null, {
         statusCode: '200',
